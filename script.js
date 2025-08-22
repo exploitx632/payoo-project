2499c7589409aba1fdb55907b210b9e0870d6d3f
Md Shaon Rana <142339875+exploitx632@users.noreply.github.com>
2025-08-23 06:16:43 +0800

    added event listener to go home page

---
+++ script.js
@@ -0,0 +1,21 @@
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const mobile = 12345678910;
+    const pin = 1234;
+
+
+
+    const mobileNumberValue = document.getElementById("mobile-number").value;
+    const mobileNumber = parseInt(mobileNumberValue);
+
+    const pinNumberValue = document.getElementById("pin-number").value;
+    const pinNumber = parseInt(pinNumberValue);
+
+    if (mobileNumber === mobile && pinNumber === pin) {
+      window.location.href='./home.html';
+    }else{
+        alert("invalid credentials")
+    }
+  });
