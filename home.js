8cdf0336a2986bb2ffed28cc1ca3f0af73aeafd0
Md Shaon Rana <142339875+exploitx632@users.noreply.github.com>
2025-08-24 16:15:15 +0800

    added logout feature

--- home.js
+++ home.js
@@ -1,20 +1,20 @@
+document.getElementById("logout-btn").addEventListener("click", function () {
+  window.location.href = "./index.html";
+});
+
 document
   .getElementById("addMoney-btn")
   .addEventListener("click", function (event) {
     event.preventDefault();
-    let balance = parseInt(
-      document.getElementById("balance").innerText
-    );
-
     const card = document.getElementById("cards").value;
     const cardNumber = document.getElementById("cardNumber").value;
 
     let addMoney = parseInt(document.getElementById("addMoney").value);
 
     const pin = document.getElementById("pin").value;
+    let balance = parseInt(document.getElementById("balance").innerText);
     let totalBalance = balance + addMoney;
-    
-    
-    let currentBalance = document.getElementById("balance").innerText = totalBalance;
 
+    let currentBalance = (document.getElementById("balance").innerText =
+      totalBalance);
   });
