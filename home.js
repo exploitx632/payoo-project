778d9533a0b2c670e5531dc1436ff957440b8ad6
Md Shaon Rana <142339875+exploitx632@users.noreply.github.com>
2025-08-25 00:24:37 +0800

    added payment history section

--- home.js
+++ home.js
@@ -3,42 +3,78 @@ document.getElementById("logout-btn").addEventListener("click", function () {
 });
 
 let validPin = 1234;
-// add money form 
 
-document.getElementById("add-money").addEventListener("click",function(){
-  document.getElementById("addMoney-form").classList.remove("hidden");
-  document.getElementById("cashOut-form").classList.add("hidden");
-})
-// cash out form 
-document.getElementById("cash-out").addEventListener("click",function(){
-  document.getElementById("cashOut-form").classList.remove("hidden");
-  document.getElementById("addMoney-form").classList.add("hidden");});
+// featured Elements
+const homeSection = document.getElementById('home')
+const addMoney = document.getElementById("add-money");
+const addMoneyForm = document.getElementById("addMoney-form");
+const addMoneyBtn = document.getElementById("addMoney-btn");
 
+const cashOut = document.getElementById("cash-out");
+const cashOutForm = document.getElementById("cashOut-form");
+const cashOutBtn = document.getElementById("cash-out-btn");
 
-document
-  .getElementById("addMoney-btn")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    const card = document.getElementById("cards").value;
-    const cardNumber = document.getElementById("cardNumber").value;
+// add money form
+addMoney.addEventListener("click", function () {
+  addMoneyForm.classList.remove("hidden");
+  cashOutForm.classList.add("hidden");
+  homeSection.classList.add("hidden");
+});
+// add money btn
+addMoneyBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  let balance = parseInt(document.getElementById("balance").innerText);
+  const card = document.getElementById("cards").value;
+  const cardNumber = document.getElementById("cardNumber").value;
 
-    let amount = parseInt(document.getElementById("addMoney").value);
+  let amount = parseInt(document.getElementById("addMoney").value);
 
-    const pin = parseInt(document.getElementById("pin").value);
-    let balance = parseInt(document.getElementById("balance").innerText);
-    let totalBalance = balance + amount;
+  if (cardNumber.length != 16) {
+    alert("Invalid Card Number");
+    return;
+  }
+  const pin = parseInt(document.getElementById("pin").value);
 
-    console.log(card);
-    
-    if (cardNumber.length != 16) {
-      alert("Invalid Card Number");
-      return;
-    }
-    if (pin !== validPin) {
-      alert("Invalid Pin");
-      return;
-    }
-    let currentBalance = (document.getElementById("balance").innerText =
-      totalBalance);
-  });
-  
+  if (pin !== validPin) {
+    alert("Invalid Pin");
+    return;
+  }
+
+  let totalBalance = balance + amount;
+  document.getElementById("balance").innerText = totalBalance;
+  // document.getElementById('addMoney');
+});
+// cash out form
+cashOut.addEventListener("click", function () {
+  addMoneyForm.classList.add("hidden");
+  homeSection.classList.add("hidden");
+  cashOutForm.classList.remove("hidden");
+});
+
+//cash out btn
+
+cashOutBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  let balance = parseInt(document.getElementById("balance").innerText);
+  const agentNumber = document.getElementById("AgentNumber").value;
+  const cashOutAmount = parseInt(document.getElementById("cashOut").value);
+  if(balance === 0){
+    alert('Not available balance');
+    return;
+  }
+  if (agentNumber.length < 16) {
+    alert("Invalid Agent");
+    return;
+  }
+
+  const cashPin = parseInt(document.getElementById("cashOutPin").value);
+  if (cashPin !== validPin) {
+    alert("Invalid Pin");
+    return;
+  }
+
+  let totalBalance = balance - cashOutAmount;
+  document.getElementById("balance").innerText = totalBalance;
+
+  document.getElementById('cashOut').value = 0;
+});
