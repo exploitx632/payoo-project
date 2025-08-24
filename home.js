7c78c76c29d5783e797b75a526e6acae46c7331c
Md Shaon Rana <142339875+exploitx632@users.noreply.github.com>
2025-08-24 22:50:22 +0800

    cashout feature added

--- home.js
+++ home.js
@@ -2,6 +2,19 @@ document.getElementById("logout-btn").addEventListener("click", function () {
   window.location.href = "./index.html";
 });
 
+let validPin = 1234;
+// add money form 
+
+document.getElementById("add-money").addEventListener("click",function(){
+  document.getElementById("addMoney-form").classList.remove("hidden");
+  document.getElementById("cashOut-form").classList.add("hidden");
+})
+// cash out form 
+document.getElementById("cash-out").addEventListener("click",function(){
+  document.getElementById("cashOut-form").classList.remove("hidden");
+  document.getElementById("addMoney-form").classList.add("hidden");});
+
+
 document
   .getElementById("addMoney-btn")
   .addEventListener("click", function (event) {
@@ -9,12 +22,23 @@ document
     const card = document.getElementById("cards").value;
     const cardNumber = document.getElementById("cardNumber").value;
 
-    let addMoney = parseInt(document.getElementById("addMoney").value);
+    let amount = parseInt(document.getElementById("addMoney").value);
 
-    const pin = document.getElementById("pin").value;
+    const pin = parseInt(document.getElementById("pin").value);
     let balance = parseInt(document.getElementById("balance").innerText);
-    let totalBalance = balance + addMoney;
+    let totalBalance = balance + amount;
 
+    console.log(card);
+    
+    if (cardNumber.length != 16) {
+      alert("Invalid Card Number");
+      return;
+    }
+    if (pin !== validPin) {
+      alert("Invalid Pin");
+      return;
+    }
     let currentBalance = (document.getElementById("balance").innerText =
       totalBalance);
   });
+  
